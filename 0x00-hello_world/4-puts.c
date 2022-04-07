601b6b972d9784b43dcda89832be506759095c69
babaloko <jacksonsamuel880@gmail.com>
2022-04-07 16:10:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-include <stdio.h>
-
-/**
-  * main - Entry point
-  *
-  */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-} 
+ #include (stdio.h)
+  2
+  3 /**
+  4  * main - Entry point
+  5  *
+  6  * Return: Always 0 (Success)
+  7  */
+  8 int main(void)
+  9 {
+ 10 puts("\"Programming is like building a multilingual puzzle");
+ 11 return (0);
+ 12 }
